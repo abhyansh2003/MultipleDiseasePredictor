8d57c9d8c673ebe3304f7531d8b6f8f2eb1de0e1
Abhyansh Shrivastava <201B013@juetguna.in>
2024-04-15 10:31:58 +0530

    Files updated

--- app.py
+++ app.py
@@ -2,64 +2,87 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# loading saved models
+# loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/Abhyansh/Desktop/MDPS/multiple-disease-prediction-streamlit-app/saved_models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('saved_models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/Abhyansh/Desktop/MDPS/multiple-disease-prediction-streamlit-app/saved_models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('saved_models/heart_disease_model.sav', 'rb'))
 
-parkinson_disease_model = pickle.load(open('C:/Users/Abhyansh/Desktop/MDPS/multiple-disease-prediction-streamlit-app/saved_models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('saved_models/parkinsons_model.sav', 'rb'))
 
-# sidebar for navigates
-
+# sidebar for navigation
 with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',['Diabetes Prediction',
-         'Heart Disease Prediction', 'Parkinson Disease Prediction'],icons=['activity', 'heart', 'person'],
-         default_index = 0)
-    
+    selected = option_menu('Multiple Disease Prediction System',
 
-if(selected == "Diabetes Prediction"):
+                           ['Diabetes Prediction',
+                            'Heart Disease Prediction',
+                            'Parkinsons Prediction'],
+                           menu_icon='hospital-fill',
+                           icons=['activity', 'heart', 'person'],
+                           default_index=0)
+
+
+# Diabetes Prediction Page
+if selected == 'Diabetes Prediction':
+
+    # page title
     st.title('Diabetes Prediction using ML')
 
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
 
     with col1:
         Pregnancies = st.text_input('Number of Pregnancies')
+
     with col2:
         Glucose = st.text_input('Glucose Level')
+
     with col3:
-        BloodPressure = st.text_input('Blood Pressure Value')
+        BloodPressure = st.text_input('Blood Pressure value')
+
     with col1:
-        SkinThickness = st.text_input('Skin Thickness Value')
+        SkinThickness = st.text_input('Skin Thickness value')
+
     with col2:
         Insulin = st.text_input('Insulin Level')
+
     with col3:
-        BMI = st.text_input('BMI Value')
+        BMI = st.text_input('BMI value')
+
     with col1:
-        DiabetesPedegreeFunction = st.text_input('Diabetes Pedegree Function Value')
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+
     with col2:
         Age = st.text_input('Age of the Person')
 
-    # code for prediction
+
+    # code for Prediction
     diab_diagnosis = ''
 
-    # creating a button for prediction
-    
-    if st.button('Diabetes Test Result'):
-        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness,
-                                             Insulin, BMI, DiabetesPedegreeFunction, Age]])
-        if(diab_prediction[0] == 1):
-            # diab_diagnosis = 'The Person is Diabetic'
-            diab_diagnosis=st.markdown(':red[The Person is Diabetic]')
+    # creating a button for Prediction
 
+    if st.button('Diabetes Test Result'):
+
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age]
+
+        user_input = [float(x) for x in user_input]
+
+        diab_prediction = diabetes_model.predict([user_input])
+
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
         else:
-            diab_diagnosis = st.markdown(':green[The Person is not Diabetic]')
-    
+            diab_diagnosis = 'The person is not diabetic'
+
     st.success(diab_diagnosis)
 
-if(selected == "Heart Disease Prediction"):
+# Heart Disease Prediction Page
+if selected == 'Heart Disease Prediction':
+
+    # page title
     st.title('Heart Disease Prediction using ML')
-    
+
     col1, col2, col3 = st.columns(3)
 
     with col1:
@@ -115,14 +138,17 @@ if(selected == "Heart Disease Prediction"):
         heart_prediction = heart_disease_model.predict([user_input])
 
         if heart_prediction[0] == 1:
-            heart_diagnosis = st.markdown(':red[The person is having heart disease]')
+            heart_diagnosis = 'The person is having heart disease'
         else:
-            heart_diagnosis = st.markdown(':green[The person does not have any heart disease]')
+            heart_diagnosis = 'The person does not have any heart disease'
 
     st.success(heart_diagnosis)
 
-if(selected == "Parkinson Disease Prediction"):
-    st.title('Parkinson Disease Prediction using ML')
+# Parkinson's Prediction Page
+if selected == "Parkinsons Prediction":
+
+    # page title
+    st.title("Parkinson's Disease Prediction using ML")
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
